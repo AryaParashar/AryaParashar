a91a9f36df28e40556d04307436674c5baa3aa73
hp <hp@ITSSSSS>
2021-08-04 22:22:02 -0700

    commit changes to local branch

---
+++ OnlineExam/Controllers/AdminController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineExam.Controllers
+{
+    public class AdminController : Controller
+    {
+        public OnlineExamEntities OE = new OnlineExamEntities();
+        // GET: Admin
+        public ActionResult AdminDashboard()
+        {
+
+            return View();
+        }
+        public ActionResult AddCourse()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCourse(tblCours Course)
+        {
+            OE.tblCourses.Add(Course);
+            OE.SaveChanges();
+            return View();
+        }
+
+        public ActionResult AddSubject()
+        {
+            tblSubject MD = new tblSubject();
+            MD.CourseList = new SelectList(OE.tblCourses, "CourseID", "CourseName"); // model binding
+            return View(MD);
+        }
+
+        [HttpPost]
+        public ActionResult AddSubject(tblSubject subject)
+        {
+            OE.tblSubjects.Add(subject);
+            OE.SaveChanges();
+            //return View();
+            return RedirectToAction("AddQuestions");
+        }
+        public ActionResult AddQuestions()
+        {
+            tblQuestion QUE = new tblQuestion();
+            QUE.SubjectList = new SelectList(OE.tblSubjects, "SubjectID", "SubjectName"); // model binding
+            return View(QUE);
+        }
+        [HttpPost]
+        public ActionResult AddQuestions(tblQuestion qes)
+        {
+            OE.tblQuestions.Add(qes);
+            OE.SaveChanges();
+            return View();
+        }
+
+    }
+}
---
+++ OnlineExam/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineExam.Controllers
+{
+    public class HomeController : Controller
+    {
+        private OnlineExamEntities ER = new OnlineExamEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(tblUserRegister UR)
+        {
+            ER.tblUserRegisters.Add(UR);
+            ER.SaveChanges();
+            Int32 UserId = UR.UserId;
+            Response.Write(UserId);
+            return View();
+        }
+
+        public ActionResult UsersList()
+        {
+            return View(from record in ER.UsersViews select record);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(Int32 UserId)
+        {
+            ER.sp_UserDelete(UserId);
+            return RedirectToAction("UsersList");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(Int32 UserId)
+        {
+            tblUserRegister UR = ER.tblUserRegisters.Find(UserId);
+            return View(UR);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(tblUserRegister UR)
+        {
+            //ER.sp_Update_UserInfo(UR.UserId, UR.UserName, UR.Password, UR.Email, UR.DOB);
+            return View(UR);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(tblUserRegister UR)
+        {
+            //int? UserId = ER.ValidateUser(UR.UserName, UR.Password).FirstOrDefault();
+            var user = ER.UsersViews.Where(u => u.UserName == UR.UserName && u.Password == UR.Password).FirstOrDefault();
+            return View();
+        }
+        //public ActionResult Admin()
+        //{
+        //    string apiUri = Url.HttpRouteUrl("DefaultApi", new { controller = "Admin", });
+        //    ViewBag.ApiUrl = new Uri(Request.Url, apiUri).AbsoluteUri.ToString();
+
+        //    return View();
+        //}
+
+    }
+}
